947c4f6abc7a4d1529e216359f3532bac3dd7d8b
Lenon <lenonsdp@hotmail.com>
2021-03-13 12:14:39 -0300

    Inicio do projeto, criaçao de estrutura

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -12,67 +12,67 @@ class CategoriaController extends Controller
 		$this->categoria = $categoria;
 	}
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        $categorias = $this->categoria->all();
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index()
+	{
+		$categorias = $this->categoria->all();
 		return $categorias;
-    }
+	}
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-       $categoria = $this->categoria->create($request->all());
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(Request $request)
+	{
+	   $categoria = $this->categoria->create($request->all());
 	   return $categoria;
-    }
+	}
 
-    /**
-     * Display the specified resource.
-     *
-     * @param Integer
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param Integer
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show($id)
+	{
 		$categoria = $this->categoria->find($id);
-        return $categoria;
-    }
+		return $categoria;
+	}
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  Integer
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  Integer
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(Request $request, $id)
+	{
 		$categoria = $this->categoria->find($id);
-        $categoria->update($request->all());
+		$categoria->update($request->all());
 
 		return $categoria;
-    }
+	}
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  Integer
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        $categoria = $this->categoria->find($id);
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  Integer
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy($id)
+	{
+		$categoria = $this->categoria->find($id);
 		$categoria->delete();
 
 		return ['success' => true];
-    }
+	}
 }
