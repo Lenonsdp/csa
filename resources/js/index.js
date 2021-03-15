495a5c270900652eda0d91bbb3918c055f832326
Lenon <lenonsdp@hotmail.com>
2021-03-14 21:46:44 -0300

    Criação de listagem de produto objeto jquery;

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,13 +1,25 @@
+var protucts;
+
 $(function() {
-	$('#menu-toggle').on('click', function(e) {
-		e.preventDefault();
-		if($('#wrapper').hasClass('active')) {
-			$('#sidebar').hide();
-			$('#ulToggleDisabled').show();
-		} else {
-			$('#sidebar').show();
-			$('#ulToggleDisabled').hide();
-		}
-		$('#wrapper').toggleClass('active');
-	});
+	protucts = new Protucts('painel_base');
 });
+
+var Protucts = function () {
+	this.registerEvents();
+}
+
+Protucts.prototype = {
+	'registerEvents': function() {
+		$('#menu-toggle').on('click', function(e) {
+			e.preventDefault();
+			if($('#wrapper').hasClass('active')) {
+				$('#sidebar').hide();
+				$('#ulToggleDisabled').show();
+			} else {
+				$('#sidebar').show();
+				$('#ulToggleDisabled').hide();
+			}
+			$('#wrapper').toggleClass('active');
+		});
+	}
+}
